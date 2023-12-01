f09beb233f3a9cc4fab9e1eefb1816e85c945b5e
李江辰 <SpaceCode@foxmail.com>
2023-12-01 21:36:00 +0800

    perf: 优化项目结构

--- config/index.ts
+++ config/index.ts
@@ -6,24 +6,13 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 import removeConsole from 'vite-plugin-remove-console';
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 import configCompressPlugin from './plugin/compress';
-import configArcoStyleImportPlugin from './plugin/arcoStyleImport';
 import configVisualizerPlugin from './plugin/visualizer';
-import AutoImport from 'unplugin-auto-import/vite';
-import Components from 'unplugin-vue-components/vite';
-import { ArcoResolver } from 'unplugin-vue-components/resolvers';
+import autoImportPlugin from './plugin/autoImport';
 
 export function pluginsList(): PluginOption[] {
   return [
     vue(),
-    AutoImport({
-      imports: [],
-      resolvers: [ArcoResolver()],
-      dts: './src/types/auto-imports.d.ts',
-    }),
-    Components({
-      extensions: ['vue'],
-      resolvers: [ArcoResolver()],
-    }),
+    autoImportPlugin(),
     VueI18nPlugin({
       runtimeOnly: true,
       compositionOnly: true,
@@ -32,7 +21,6 @@ export function pluginsList(): PluginOption[] {
     vueJsx(), // jsx支持
     removeConsole(), // 移除console
     svgLoader({ svgoConfig: {} }), // svg组件化支持
-    configArcoStyleImportPlugin(), // Arco官方插件
     configCompressPlugin('gzip'), // 压缩
     configVisualizerPlugin('none'), // 打包分析
   ];
---
+++ config/plugin/autoImport.ts
@@ -0,0 +1,22 @@
+/**
+ * 按需引入
+ * https://github.com/antfu/unplugin-vue-components
+ */
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ArcoResolver } from 'unplugin-vue-components/resolvers';
+
+export default function autoImportPlugin() {
+  return [
+    AutoImport({
+      imports: [],
+      resolvers: [ArcoResolver()],
+      dts: './src/types/auto-imports.d.ts',
+    }),
+    Components({
+      extensions: ['vue'],
+      resolvers: [ArcoResolver()],
+      dts: './src/types/components.d.ts',
+    }),
+  ];
+}
