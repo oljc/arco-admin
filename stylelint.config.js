7e5614be68b7731a57776c6ec33f6132b74d878c
李江辰 <SpaceCode@foxmail.com>
2023-12-17 20:33:46 +0800

    chore: 更新stylelint至最新版ESM

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,25 +1,24 @@
 /** @type {import("stylelint").Config} */
-module.exports = {
-  root: true,
+export default {
   defaultSeverity: 'warning',
   extends: [
-    "stylelint-config-standard",
-    "stylelint-config-html/vue",
-    "stylelint-config-recess-order"
+    'stylelint-config-standard',
+    'stylelint-config-html/vue',
+    'stylelint-config-recess-order',
   ],
   plugins: ['stylelint-order', 'stylelint-less', 'stylelint-prettier'],
   rules: {
     // 禁止空代码
     'no-empty-source': null,
     // 禁止在覆盖高特异性选择器之后出现低特异性选择器
-    "no-descending-specificity": null,
+    'no-descending-specificity': null,
     // 不允许未知单位
     'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
     // 禁止小于 1 的小数有一个前导零
     // "number-leading-zero": "never",
-    "media-query-no-invalid": null,
+    'media-query-no-invalid': null,
     // 禁止空注释
-    "comment-no-empty": true,
+    'comment-no-empty': true,
     // 未知的 @ 规则
     'at-rule-no-unknown': [
       true,
@@ -35,7 +34,7 @@ module.exports = {
           'mixin',
           'extend',
           'content',
-          'use'
+          'use',
         ],
       },
     ],
@@ -43,26 +42,26 @@ module.exports = {
     'rule-empty-line-before': [
       'always',
       {
-        ignore: ["after-comment", "first-nested"]
+        ignore: ['after-comment', 'first-nested'],
       },
     ],
-    "order/order": [
+    'order/order': [
       [
-        "dollar-variables",
-        "custom-properties",
-        "at-rules",
-        "declarations",
+        'dollar-variables',
+        'custom-properties',
+        'at-rules',
+        'declarations',
         {
-          type: "at-rule",
-          name: "supports"
+          type: 'at-rule',
+          name: 'supports',
         },
         {
-          type: "at-rule",
-          name: "media"
+          type: 'at-rule',
+          name: 'media',
         },
-        "rules"
+        'rules',
       ],
-      { severity: "warning" }
+      { severity: 'warning' },
     ],
   },
   overrides: [
@@ -71,9 +70,15 @@ module.exports = {
       customSyntax: 'postcss-html',
       rules: {
         // 禁止未知的伪类选择器
-        'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['deep', 'global'] }],
+        'selector-pseudo-class-no-unknown': [
+          true,
+          { ignorePseudoClasses: ['deep', 'global'] },
+        ],
         // 禁止未知的伪元素选择器
-        'selector-pseudo-element-no-unknown': [true, { ignorePseudoElements: ['v-deep', 'v-global', 'v-slotted'] }],
+        'selector-pseudo-element-no-unknown': [
+          true,
+          { ignorePseudoElements: ['v-deep', 'v-global', 'v-slotted'] },
+        ],
       },
     },
     {
