df204cefd8ad8b2b628d9cbc8bb352b4fb936847
李江辰 <SpaceCode@foxmail.com>
2023-12-29 18:59:29 +0800

    feat: 底部版权说明调整

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -1,6 +1,15 @@
 <template>
   <footer class="footer">
-    <slot>Arco Admin</slot>
+    <slot>
+      Arco Admin 版权所有 © 2023 李江辰｜
+      <a-link
+        style="font-size: 12px"
+        href="https://github.com/LIjiAngChen8/arco-admin/blob/main/LICENSE"
+        target="_blank"
+      >
+        MIT 使用条款
+      </a-link>
+    </slot>
   </footer>
 </template>
 
@@ -10,6 +19,7 @@
     align-items: center;
     justify-content: center;
     height: 40px;
+    font-size: @size-mini;
     color: var(--color-text-2);
     text-align: center;
   }
