fd01af859aec293ec2f31e2b3915346d5e76378a
李江辰 <SpaceCode@foxmail.com>
2024-01-26 12:18:35 +0800

    chore: 调整默认路由

--- src/router/constants.ts
+++ src/router/constants.ts
@@ -14,5 +14,5 @@ export const DEFAULT_ROUTE_NAME = 'Workplace';
 export const DEFAULT_ROUTE = {
   title: 'menu.dashboard.workplace',
   name: DEFAULT_ROUTE_NAME,
-  fullPath: '/dashboard/workplace'
+  fullPath: '/workplace'
 };
