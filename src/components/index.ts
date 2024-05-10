034752349ee4a248b1a00d1b845e0a72e39fb6b9
李江辰 <SpaceCode@foxmail.com>
2024-05-10 11:39:16 +0800

    feat: 字节visactor替换Echart

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,27 +1,4 @@
 import type { App } from 'vue';
-import { use } from 'echarts/core';
-import { CanvasRenderer } from 'echarts/renderers';
-import { BarChart, LineChart, PieChart, RadarChart } from 'echarts/charts';
-import {
-  GridComponent,
-  TooltipComponent,
-  LegendComponent,
-  DataZoomComponent,
-  GraphicComponent
-} from 'echarts/components';
-
-use([
-  CanvasRenderer,
-  BarChart,
-  LineChart,
-  PieChart,
-  RadarChart,
-  GridComponent,
-  TooltipComponent,
-  LegendComponent,
-  DataZoomComponent,
-  GraphicComponent
-]);
 
 export default {
   install(_Vue: App) {}
