1f6daec7360a68b4655b0d8d14116232bc43ff05
李江辰 <SpaceCode@foxmail.com>
2024-05-13 16:56:19 +0800

    chore: 调整 navbar 标题名

--- src/components/navbar/index.vue
+++ src/components/navbar/index.vue
@@ -7,7 +7,7 @@
           :style="{ margin: 0, fontSize: '18px' }"
           :heading="5"
         >
-          Arco Pro
+          Arco Admin
         </a-typography-title>
         <icon-menu-fold
           v-if="!topMenu && appStore.device === 'mobile'"
